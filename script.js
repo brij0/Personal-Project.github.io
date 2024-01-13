8d4f429505f60112be3b6a3385ba6482bbd9a744
brij0 <brijeshthakrar03@gmail.com>
2024-01-13 13:24:17 -0500

    Added New Files

---
+++ script.js
@@ -0,0 +1,5 @@
+function showContent(tab) {
+    // Hide all content sections
+    document.getElementById('skills-content').style.display = 'none';
+    document.getElementById('experience-content').style.display = ''
+}
